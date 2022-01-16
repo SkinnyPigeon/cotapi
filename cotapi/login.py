8a24109ec95c650cb4f74d83f65a52f77b608668
skinnypigeon <philtheskinnypigeon@yahoo.co.uk>
2022-01-17 00:25:04 +0100

    can fully validate and login users

--- cotapi/login.py
+++ cotapi/login.py
@@ -1,15 +1,23 @@
 import os
 from passlib.context import CryptContext
-from user import UserPersistance as UP
+from user import UserPersistance as UP, User
 from typing import Optional
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 
 def verify_password(plain_password, hashed_password):
@@ -34,3 +42,30 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    up = UP(token_data.username)
+    user = up.load_user()
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
--- cotapi/tests/login_test.py
+++ cotapi/tests/login_test.py
@@ -7,6 +7,8 @@ if "/Users/euanblackledge/Desktop/code/cotapi/cotapi" not in sys.path:
 import login
 import user
 
+pytestmark = pytest.mark.asyncio
+
 
 @pytest.mark.skip("Relies on having the same password hash")
 def test_verify_works_with_valid_password():
@@ -35,6 +37,30 @@ def test_token_created():
     token = login.create_access_token({'sub': 'euan'})
     assert token[:2] == 'ey'
     assert len(token) == 123
+
+
+@pytest.mark.skip("Relies on having the same password hash")
+async def test_get_current_user_returns_user_with_valid_token():
+    token = login.create_access_token({'sub': 'euan'})
+    current_user = await login.get_current_user(token)
+    expected_user = {'username':
+                     'euan', 'disabled': False,
+                     'address': '51dbd2feecb8c9e3b5c88129da88156d738d00d57bf4524cc780221c4e414ffc9372b00ad7d75679032d928776b044d40d5febb783d8ac9b241b7c0b1cad77de9b699c23',
+                     'key': '$2b$12$XTROw3rZ8/pHzFjIalDbeOSA1neYmSI5ga9FBjMxCVeUZJQJjxw..'}
+    assert current_user == user.UserInDB(**expected_user)
+
+
+@pytest.mark.skip("Relies on having the same password hash")
+async def test_get_current_active_user_returns_valid_user():
+    token = login.create_access_token({'sub': 'euan'})
+    current_user = await login.get_current_user(token)
+    active_user = await login.get_current_active_user(current_user)
+    expected_user = {'username':
+                     'euan',
+                     'disabled': False,
+                     'address': '51dbd2feecb8c9e3b5c88129da88156d738d00d57bf4524cc780221c4e414ffc9372b00ad7d75679032d928776b044d40d5febb783d8ac9b241b7c0b1cad77de9b699c23',
+                     'key': '$2b$12$XTROw3rZ8/pHzFjIalDbeOSA1neYmSI5ga9FBjMxCVeUZJQJjxw..'}
+    assert active_user == user.UserInDB(**expected_user)
 
 
 sys.path.remove("/Users/euanblackledge/Desktop/code/cotapi/cotapi")
